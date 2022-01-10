47e2f1886ee630cea27bf571d7f169aa10157d53
omarquezpayall <omarquez@payall.com.ve>
2022-01-10 09:30:24 -0400

    Extender del reporte de la factura

---
+++ formato-factura/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'formato - factura',
+    
+    'summary': """
+        This is a module 
+    """,
+    
+    'description': """
+        This is a module 
+    """,
+    
+    'author': 'Payall',
+    
+    'website': 'https://payall.com.ve/',
+    
+    'category': 'Accounting',
+    
+    'version': '1.0.0',
+    
+    'depends': ['account_accountant'],
+    
+    'data': [
+        "report/report_invoice_document.xml"
+    ],
+    
+    'demo': [
+    ],
+}
