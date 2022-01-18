82f01e9aaafa8870d5a677fa8a9d6c16495eb485
avpayall <avpayall@gmail.com>
2022-01-18 20:28:11 +0000

    [FIX] syntax

--- formato-factura/__manifest__.py
+++ formato-factura/__manifest__.py
@@ -26,7 +26,7 @@
     'data': [
         'security/invoices_images_security.xml',
         'security/ir.model.access.csv',
-        'views/invoices_images.xml',
+        'views/invoice_images.xml',
         'views/views.xml',
         'views/templates.xml',
         'report/report_invoice_document.xml'
