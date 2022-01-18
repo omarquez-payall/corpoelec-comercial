c32dd00161ec807245e295e7d723dbdd4de7cc66
avpayall <avpayall@gmail.com>
2022-01-18 19:51:23 +0000

    [ADD] invoice images model

---
+++ formato-factura/models/.ipynb_checkpoints/invoice_images-checkpoint.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class InvoiceImages( models.Model):
+    _name = 'invoices.images'
+    _description = 'This model is to load and manage the images needed for the invoice template'
+    logo = fields.Binary(string='Logo')
+    header_logo = fields.Binary(string='Header Logo')
+    full_logo = fields.Binary(string='Full Logo')
+    payment_methods = fields.Binary(string='Payment Methods')
