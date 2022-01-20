8058f3612fca77e42db663939b8844fe5c38b493
avpayall <avpayall@gmail>
2022-01-20 16:45:44 +0000

    [FIX] syntax error

--- formato-factura/models/.ipynb_checkpoints/invoice_images_inherit-checkpoint.py
+++ formato-factura/models/.ipynb_checkpoints/invoice_images_inherit-checkpoint.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 
 class InvoiceImagesInherit( models.Model):
     _inherit = 'account.move'
-    images = fields.Many2one( string = 'Imágenes para facturas', comodel_name = 'invoices.images' default=1)
+    images = fields.Many2one( string = 'Imágenes para facturas', comodel_name = 'invoices.images', default=1)
