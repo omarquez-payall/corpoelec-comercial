2cda40ec0dfabe6e97016ccb7558801680034014
avpayall <avpayall@gmail.com>
2022-01-20 14:38:00 +0000

    [TEST] new model of importing data to the invoice report

--- formato-factura/models/invoice_images_inherit.py
+++ formato-factura/models/invoice_images_inherit.py
@@ -5,7 +5,3 @@ from odoo import models, fields, api
 class InvoiceImagesInherit( models.Model):
     _inherit = 'account.move'
     images = fields.Many2one( string = 'Imágenes para facturas', comodel_name = 'invoices.images')
-    logo = fields.Binary(string='Logo', related='images.logo')
-    header_logo = fields.Binary(string='Header Logo', related='images.header_logo')
-    full_logo = fields.Binary(string='Full Logo', related='images.full_logo')
-    payment_methods = fields.Binary(string='Payment Methods', related='images.payment_methods')
