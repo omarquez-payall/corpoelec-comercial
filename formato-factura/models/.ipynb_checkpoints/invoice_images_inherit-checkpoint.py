5047d3629f8daa925cd3c0412380fcc3e56ced81
avpayall <avpayall@gmail.com>
2022-01-20 15:23:40 +0000

    [TEST] database search

--- formato-factura/models/.ipynb_checkpoints/invoice_images_inherit-checkpoint.py
+++ formato-factura/models/.ipynb_checkpoints/invoice_images_inherit-checkpoint.py
@@ -5,3 +5,8 @@ from odoo import models, fields, api
 class InvoiceImagesInherit( models.Model):
     _inherit = 'account.move'
     images = fields.Many2one( string = 'Imágenes para facturas', comodel_name = 'invoices.images')
+    
+    @api.model
+    def get_data_from_invoice_images(self):
+        for record in self:
+            record.images = self.env['invoices.images'].search([('id','=', 1)])
