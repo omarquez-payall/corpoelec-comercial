e31a6f68b28c68596b78e530862eaac86c6bcb2c
avpayall <avpayall@gmail.com>
2022-01-25 13:38:51 +0000

    [TEST] removing sequence methods

--- formato-factura/models/res_partner.py
+++ formato-factura/models/res_partner.py
@@ -4,16 +4,16 @@ from odoo import models, fields, api
 
 class PartnerCodeInherit( models.Model):
     _inherit = 'res.partner'
-    partner_code = fields.Char(string = 'Código de Interlocutor', default=lambda self: self._get_next_sequence_number())
+    partner_code = fields.Char(string = 'Código de Interlocutor')
     
-    @api.model
-    def create(self, vals):
-        vals['partner_code'] = self.env['ir.sequence'].next_by_code('partner_code_seq')
-        result = super( PartnerCodeInherit, self).create(vals)
-        return result 
+    #@api.model
+    #def create(self, vals):
+    #    vals['partner_code'] = self.env['ir.sequence'].next_by_code('partner_code_seq')
+    #    result = super( PartnerCodeInherit, self).create(vals)
+    #    return result 
 
-    @api.model
-    def _get_next_sequence_number(self):
-        sequence = self.env['ir.sequence'].search([('code','=','partner_code_seq')])
-        next= sequence.get_next_char(sequence.number_next_actual)
-        return next
+    #@api.model
+    #def _get_next_sequence_number(self):
+    #    sequence = self.env['ir.sequence'].search([('code','=','partner_code_seq')])
+    #    next= sequence.get_next_char(sequence.number_next_actual)
+    #    return next
