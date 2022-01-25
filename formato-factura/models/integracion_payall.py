35529a6015bf0267b7a14ef7b8b15c12ca0c139d
omarquezpayall <omarquez@payall.com.ve>
2022-01-25 16:02:39 -0400

    Agregar boilerplate y prueba de consulta a traves de la API

--- formato-factura/models/integracion_payall.py
+++ formato-factura/models/integracion_payall.py
@@ -1,26 +1,49 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 
 class IntegacionPayall( models.Model):
     _name ='integracion.payall'
     _description = 'Modelo para la integracion con Payall a traves de la API'
 
-
-    def consultaDeudaCliente():
-        
-        return None
+    @api.model 
+    def consultaDeudaCliente(self, res_partner_code):
+        if (res_partner_code is None):
+            result = { 
+                "code":"C7",
+                "message":"El codigo del cliente no fue recibido correctamente por el servidor"
+            }
+            return result
+        partner = self.env['res.partner'].search([['partner_code','=',res_partner_code]])
+        result = {
+            "code":"00",
+            "amount": partner.credit
+        }
+        return result
     
     @api.model
-    def pagarDeduaCliente(self):
-        invoices = self.env['account.move'].search([['partner_id.id','=',41], ['move_type','=','out_invoice']])
+    def pagarDeduaCliente(self, amount, res_partner_code):
+        if (amount is None):
+            result = { 
+                "code":"M4",
+                "message":"El monto no fue recibido correctamente por el servidor"
+            }
+            return result
+        if ( res_partner_code is None):
+            result = { 
+                "code":"I7",
+                "message":"Identificador del cliente no fue recibido correctamente por el servidor"
+            }
+            return result
+        invoices = self.env['account.move'].search([['partner_id.id','=', res_partner_code], ['move_type','=','out_invoice']])
         payment_method = self.env['account.payment.method'].search([['code','=','manual'],['payment_type','=','inbound']])
         active_ids = invoices.ids
         payments = self.env['account.payment.register'].with_context(active_model='account.move', active_ids=active_ids).create({
-            'amount': 30000.00,
+            'amount': amount,
             'group_payment': True,
             'payment_difference_handling': 'open',
             'currency_id': self.env.company['currency_id'].id,
             'payment_method_id': payment_method.id,
         })._create_payments()
-        return True
+        return payments.payment_difference
+ 
