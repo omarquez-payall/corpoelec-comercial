b962f4973c54a7b621bb57c0640f455e72e3cbdb
Oscar <oscarm@OscarPayall>
2022-01-29 11:52:08 -0400

    Evitar repetidos y ocultar boton

--- formato-factura/models/account_move.py
+++ formato-factura/models/account_move.py
@@ -13,6 +13,7 @@ class AccountMove( models.Model):
     #    inverse_name = "factura_id")
 
     dias_lectura = fields.Integer( string = "Dias Lectura")
+    cargar_productos = field.Boolean( string="Cargar", default = False)
     # COMO DEBE ESTAR EN PRODUCCION
     #dias_lectura = fields.Integer( string = "Dias Lectura", required = True)
 
@@ -22,14 +23,15 @@ class AccountMove( models.Model):
     def cargar_productos_electricidad(self):
         for record in self:
             products = self.env['product.product'].search( [['precargar','=',True]])
-            self.narration = products
-            for product in products:
-                #CAMBIAR ACCOUNT_ID CUANDO SE SEPA A CUAL VA
-                self.invoice_line_ids.create({
-                    'name': product.name,
-                    'price_unit': product.price,
-                    'quantity': 1,
-                    'product_id': product.id,
-                    'account_id': 1,
-                    'move_id': self.id
-                })
+            if products not in record.invoice_line_ids: 
+                for product in products:
+                    #CAMBIAR ACCOUNT_ID CUANDO SE SEPA A CUAL VA
+                    record.invoice_line_ids.create({
+                        'name': product.name,
+                        'price_unit': product.price,
+                        'quantity': 1,
+                        'product_id': product.id,
+                        'account_id': 1,
+                        'move_id': record.id
+                    })
+                    record.cargar_productos = True
