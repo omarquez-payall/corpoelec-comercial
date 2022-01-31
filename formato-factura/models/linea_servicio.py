0074c6a453a3aaec10d0445b6c1ce93e1c5d2cc2
omarquezpayall <omarquez@payall.com.ve>
2022-01-31 13:47:20 -0400

    Agregar regla de acceso

--- formato-factura/models/linea_servicio.py
+++ formato-factura/models/linea_servicio.py
@@ -6,8 +6,6 @@ class LineaServicio( models.Model):
     _name = 'linea.servicio'
     _description = 'Modelo para la linea de los servicios'
 
-
-
     move_id = fields.Many2one( comodel_name = "account.move", string = "Linea de Servicio")
     nombre_cargo = fields.Char( string = "Cargo")
     cantidad = fields.Integer( string = "Cantidad")
