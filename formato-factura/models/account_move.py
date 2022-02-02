02cddfe0532135c71a8c4bb57adb39dfac1a31b8
Oscar <oscarm@OscarPayall>
2022-02-01 21:02:56 -0400

    Volver con las sequencias

--- formato-factura/models/account_move.py
+++ formato-factura/models/account_move.py
@@ -6,8 +6,8 @@ class AccountMove( models.Model):
     _inherit = 'account.move'
 
     #------------------- Relacion con los servicios ------------------
-    No_Contable = fields.Char( string = 'No Doc Contable',readonly=True, required = True, index=True, default=lambda self: self._get_next_sequence_number_contable())
-    No_Registro = fields.Char( string = 'No Registro',readonly=True, required = True, index=True, default=lambda self: self._get_next_sequence_number_registro())
+    No_Contable = fields.Char( string = 'No Doc Contable',readonly=True, required = True, index=True, default=lambda self: _('New'))
+    No_Registro = fields.Char( string = 'No Registro',readonly=True, required = True, index=True, default=lambda self: _('New'))
     
     inicio_periodo = fields.Date(string='Inicio período', default=fields.Date.today, store=True)
     fin_periodo = fields.Date(string='Fin período', default=fields.Date.today, store=True)
@@ -25,8 +25,8 @@ class AccountMove( models.Model):
 
     @api.model
     def create(self, vals):
-        vals['No_Contable'] = self.env['ir.sequence'].next_by_code('Seq_No_Contable')
-        vals['No_Registro'] = self.env['ir.sequence'].next_by_code('Seq_No_Registro')
+        vals['No_Contable'] = self.env['ir.sequence'].next_by_code('Seq_No_Contable') or _('New')
+        vals['No_Registro'] = self.env['ir.sequence'].next_by_code('Seq_No_Registro') or _('New')
         result = super(AccountMove, self).create(vals)
         return result 
 
